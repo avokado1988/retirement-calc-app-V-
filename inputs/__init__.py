b596798f5d1d617d1e5baea72384196b89d9ccdf
avokado1988 <55538227+avokado1988@users.noreply.github.com>
2026-05-25 23:20:47 +0300

    Update __init__.py

--- inputs/__init__.py
+++ inputs/__init__.py
@@ -19,17 +19,19 @@ def render_all_sidebar_inputs():
     with st.sidebar.expander("2. נתוני הון ונדל\"ן", expanded=False):
         inputs_dict["wealth"] = render_wealth_inputs()
         
+    # שמירת נתון ההון ההתחלתי המחושב לשימוש במסלולים השונים
+    remaining_wealth = inputs_dict["wealth"]["remaining_for_gimel"]
+        
     # 3. קריאה לסליידרים של תקציב והוצאות
     with st.sidebar.expander("3. תקציב והוצאות", expanded=False):
         inputs_dict["expenses"] = render_expenses_inputs()
         
-    # 4. קריאה לסליידרים של מסלול תיקון 190
+    # 4. קריאה לסליידרים של מסלול תיקון 190 (מעבירים את ההון)
     with st.sidebar.expander("4. מסלול תיקון 190", expanded=False):
-        remaining_wealth = inputs_dict["wealth"]["remaining_for_gimel"]
         inputs_dict["amendment_190"] = render_190_inputs(remaining_wealth)
         
-    # 5. קריאה לסליידרים של מסלול 25% מס ריאלי
+    # 5. קריאה לסליידרים של מסלול 25% מס ריאלי (התיקון: מעבירים את ההון גם כאן!)
     with st.sidebar.expander("5. מסלול 25% מס ריאלי", expanded=False):
-        inputs_dict["real_tax_25"] = render_25_inputs()
+        inputs_dict["real_tax_25"] = render_25_inputs(remaining_wealth)
         
     return inputs_dict
