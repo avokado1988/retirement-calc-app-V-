507a93582cfb8d18acf88f5f6d1e6a1b4cea1dc8
avokado1988 <55538227+avokado1988@users.noreply.github.com>
2026-05-26 19:06:35 +0300

    Update __init__.py

--- inputs/__init__.py
+++ inputs/__init__.py
@@ -2,6 +2,7 @@ import streamlit as st
 from .timeline import render_timeline_inputs
 from .wealth import render_wealth_inputs
 from .expenses import render_expenses_inputs
+from .incomes import render_incomes_inputs
 from .amendment_190 import render_190_inputs
 from .real_tax_25 import render_25_inputs
 
@@ -11,27 +12,30 @@ def render_all_sidebar_inputs():
     
     inputs_dict = {}
     
-    # 1. קריאה לסליידרים של זמנים וגילאים
     with st.sidebar.expander("1. נתוני זמנים ופרישה", expanded=True):
         inputs_dict["timeline"] = render_timeline_inputs()
     
-    # 2. קריאה לסליידרים של נתוני הון ונדל"ן
     with st.sidebar.expander("2. נתוני הון ונדל\"ן", expanded=False):
         inputs_dict["wealth"] = render_wealth_inputs()
         
-    # שמירת נתון ההון ההתחלתי המחושב לשימוש במסלולים השונים
     remaining_wealth = inputs_dict["wealth"]["remaining_for_gimel"]
+    
+    with st.sidebar.expander("3. מקורות הכנסה", expanded=False):
+        incomes_ui = render_incomes_inputs()
         
-    # 3. קריאה לסליידרים של תקציב והוצאות
-    with st.sidebar.expander("3. תקציב והוצאות", expanded=False):
-        inputs_dict["expenses"] = render_expenses_inputs()
+    with st.sidebar.expander("4. תקציב והוצאות", expanded=False):
+        expenses_ui = render_expenses_inputs()
         
-    # 4. קריאה לסליידרים של מסלול תיקון 190 (מעבירים את ההון)
-    with st.sidebar.expander("4. מסלול תיקון 190", expanded=False):
+    # --- הזרקת ההכנסות חזרה למקומן המקורי עבור המנוע ---
+    inputs_dict["wealth"]["national_insurance"] = incomes_ui["national_insurance"]
+    expenses_ui["work_income"] = incomes_ui["work_income"]
+    expenses_ui["work_end_age"] = incomes_ui["work_end_age"]
+    inputs_dict["expenses"] = expenses_ui
+    
+    with st.sidebar.expander("5. מסלול תיקון 190", expanded=False):
         inputs_dict["amendment_190"] = render_190_inputs(remaining_wealth)
         
-    # 5. קריאה לסליידרים של מסלול 25% מס ריאלי (התיקון: מעבירים את ההון גם כאן!)
-    with st.sidebar.expander("5. מסלול 25% מס ריאלי", expanded=False):
+    with st.sidebar.expander("6. מסלול 25% מס ריאלי", expanded=False):
         inputs_dict["real_tax_25"] = render_25_inputs(remaining_wealth)
         
     return inputs_dict
